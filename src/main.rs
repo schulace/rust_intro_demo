3554516b49bbcbba63cac61bfda2726a2709b475
Alex Schuler <schulace@gmail.com>
2020-06-11 11:07:18 -0700

    the (better) fill in the blanks

--- src/main.rs
+++ src/main.rs
@@ -72,8 +72,8 @@ impl HiveGraphqlClient {
      * HeaderMap implements FromIterator<Item=(HeaderName, HeaderValue)>
      */
     let default_headers: HeaderMap = vec![
-      unimplemented!(),
-      unimplemented!(),
+      todo!(),
+      todo!(),
     ].into_iter().collect();
     
     HiveGraphqlClient {
@@ -84,20 +84,20 @@ impl HiveGraphqlClient {
     let client = &self.client;
     //use the client to POST to the endpoint, set the body to the query, send the query, and receive it as JSON
     // which will parse directly into an ActionQueryResponse
-    let res_data: ActionsQueryResponse = unimplemented!();
+    let res_data: ActionsQueryResponse = todo!();
 
     // return an Ok with an iterator over the action nodes
     Ok(res_data.data.actions.edges.into_iter()
       // go from { node: {_id, deadline, ...}} to {_id, deadline, ...}
       .map(|q_node| q_node.node)
       // return only results where the deadline exists and is after right now
-      .filter(|node| unimplemented!()))
+      .filter(|node| todo!()))
   }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  let client = HiveGraphqlClient::new("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJiQnRCdFA2eEV3WnJCZ3JaTSIsImVtYWlsIjoiYWxleC5zY2h1bGVyQGhpdmUuY29tIiwiaWF0IjoxNTg5ODI4OTk3fQ.F2OzcFFhwa-JVuwub-afjivQ_1pZcRI5mLpphvdz4cM");
+  let client = HiveGraphqlClient::new(todo!("your JWT here"));
   for action in client.my_overdue_actions().await? {
     println!("{:?}", action);
   }
